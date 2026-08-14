ad13da5baad777bf83acff6f719296298da36be6
RadGG14 <gee051400@gmail.com>
2026-08-14 23:04:32 +0800

    Update getDefaultBaseData.ts

--- server/src/game-data/getDefaultBaseData.ts
+++ server/src/game-data/getDefaultBaseData.ts
@@ -28,7 +28,7 @@ export const getDefaultBaseData = (user: User, baseType: BaseType) => {
     userid: user.userid,
     cellid: -1,
     name: user.username,
-    credits: devConfig.shiny || 100000,
+    credits: devConfig.shiny || 1000,
     createtime: currentTime,
     protected: currentTime + sevenDays,
 
