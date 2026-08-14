c518e76f597f2d3d2063683578a767a9475ff289
RadGG14 <gee051400@gmail.com>
2026-08-14 22:38:47 +0800

    Update GameConfig.ts

--- server/src/config/GameConfig.ts
+++ server/src/config/GameConfig.ts
@@ -24,7 +24,7 @@ export const devConfig = {
    * Set the default amount of shiny on the user's account.
    * Must be set before creating a new record.
    */
-  shiny: 999999,
+  shiny: 15001500,
 
   /*
    * Enable or disable the debug console. Requires a client restart.
